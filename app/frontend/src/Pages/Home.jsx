60f822f002cda79a1b7e6d511772f661790df2e6
matheusnff85 <math.marinho154@gmail.com>
2023-04-18 12:57:35 -0300

    style: implementa css da pagina home

--- app/frontend/src/Pages/Home.jsx
+++ app/frontend/src/Pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logoNova from '../images/Logo Nova-04.png';
+import styled from '../Css/Home.module.css';
 
 class Home extends React.Component {
   render() {
     return (
-      <main>
-        <img src={logoNova} alt='Logo da ultracar' />
-        <Link to={ '/service' } >Criar Novo Serviço</Link>
-        <Link to={ '/manage' } >Gerenciar Serviços</Link>
+      <main className={ styled.homeMainContainer }>
+        <img src={logoNova} alt='Logo da ultracar' className={ styled.homeLogoImg } />
+        <div className={ styled.homeButtonsContainer }>
+          <button><Link to={ '/service' } className={ styled.homeLink }>Criar Novo Serviço</Link></button>
+          <button><Link to={ '/manage' } className={ styled.homeLink }>Gerenciar Serviços</Link></button>
+        </div>
       </main>
     );
   }
