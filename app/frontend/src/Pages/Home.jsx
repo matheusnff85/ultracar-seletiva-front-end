927de1adec5db00afea3843ec7a14b2addca6e7a
matheusnff85 <math.marinho154@gmail.com>
2023-04-18 11:55:05 -0300

    feat: implementa logo na pagina inicial

--- app/frontend/src/Pages/Home.jsx
+++ app/frontend/src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoNova from '../images/Logo Nova-04.png';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
-        <h2>HOME PAGE</h2>
+        <img src={logoNova} alt='Logo da ultracar' />
         <Link to={ '/service' } >Criar Novo Serviço</Link>
         <Link to={ '/manage' } >Gerenciar Serviços</Link>
       </main>
