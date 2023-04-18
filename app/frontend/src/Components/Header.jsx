84e6b9e7cb29e67940b9388b7a4848ede1daeedf
matheusnff85 <math.marinho154@gmail.com>
2023-04-18 13:12:23 -0300

    style: estiliza header e adiciona titulo e botao para voltar a homepage no mesmo

--- app/frontend/src/Components/Header.jsx
+++ app/frontend/src/Components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logoUltracar from '../images/cabecalho.png';
+import styled from '../Css/Header.module.css';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <img src={ logoUltracar } alt="logo-ultracar" />
+      <header className={ styled.headerContainer }>
+        <img src={ logoUltracar } alt="logo-ultracar" className={ styled.headerLogo }/>
+        <h2 className={ styled.headerTitle }>Ultracar</h2>
+        <Link to='/' className={ styled.headerLink }>Sair</Link>
       </header>
     );
   }
